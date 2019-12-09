52d8e34b2dd88795bc5faf9307e515a83c7c7c9c
Ana Segarra <anasegarraber@gmail.com>
2019-12-09 01:15:49 +0100

    Remove of cards array to include it on game.js

--- js/deck.js
+++ js/deck.js
@@ -1,12 +1,36 @@
 class Deck {
-	constructor() {
-		this.cards = [
-			{ name: 'diamonds', img: 'diamonds.png' },
-			{ name: 'gold', img: 'gold.png' },
-			{ name: 'silver', img: 'silver.png' },
-			{ name: 'cloth', img: 'cloth.png' },
-			{ name: 'spice', img: 'spice.png' },
-			{ name: 'leather', img: 'leather.png' }
-		];
+	constructor(elements) {
+		this.elements = elements;
+	}
+
+	shuffle() {
+		for (let i = 0; i < this.elements.length; i++) {
+			let j = Math.floor(Math.random() * i);
+			let card = this.elements[i];
+			this.elements[i] = this.elements[j];
+			this.elements[j] = card;
+		}
+		return this.elements;
+	}
+
+	dealCards() {
+		let tempArr = []; // player/machine hand of cards
+		for (let i = 0; i < 5; i++) {
+			tempArr.push(this.elements[i]);
+		}
+		this.elements = this.elements.filter((card, index) => {
+			// console.log(card, tempArr[index], card === tempArr[index]);
+			return card !== tempArr[index];
+		});
+		return tempArr;
+		// let html = '';
+		// this.cards.forEach(card => {
+		// 	// console.log(card.name);
+		// 	html += `<div class="card-container">
+		//                     <div class="card-frame" style="background-image:url('images/${card.img}')"></div>
+		//                 </div>`;
+		// });
+
+		// document.getElementById('game-board').innerHTML = html;
 	}
 }
