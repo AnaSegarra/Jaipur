3192f130b88b3d41a7d9d3b04b9ab7819078c7cd
Ana Segarra <anasegarraber@gmail.com>
2019-12-11 19:54:31 +0100

    Create board class

--- js/board.js
+++ js/board.js
@@ -1,18 +1,21 @@
-let discardPile = document.getElementById('discard-pile');
-let playerHand = document.getElementById('player-hand');
-function cardExchange() {
-	let cards = [];
-	// console.log({ origin, destination });
-	player.cards.forEach(card => {
-		if (card.classList.contains('card-chosen')) cards.push(card);
-	});
-	let cardsArr = [ ...cards ];
-	cardsArr.forEach(card => {
-		discardPile.appendChild(card);
-		card.classList.remove('card-chosen');
-		card.removeEventListener('mouseenter', player.playerIsChoosing);
-		card.removeEventListener('mouseleave', player.playerIsChoosing);
-		card.removeEventListener('click', player.cardChosen);
-		player.pickedCards = [];
-	});
+class Board {
+	constructor() {}
+	cardExchange(destination) {
+		let cards = [];
+
+		player.cards.forEach(card => {
+			if (card.classList.contains('card-chosen')) cards.push(card);
+		});
+
+		cards.forEach(card => {
+			destination.appendChild(card);
+			card.classList.remove('card-chosen');
+			card.removeEventListener('mouseenter', player.playerIsChoosing);
+			card.removeEventListener('mouseleave', player.playerIsChoosing);
+			card.removeEventListener('click', player.cardChosen);
+			player.pickedCards = [];
+		});
+	}
+
+	tokenExchange(goodType, player) {}
 }
