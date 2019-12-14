797cc1b708343bfc28adf50cf7c9c2e0aaa5f410
Ana Segarra <anasegarraber@gmail.com>
2019-12-14 12:57:50 +0100

    Add chooseAction method and fix bug in takeCard method
    Include chooseAction method so that the machine executes one random action. Fix bug in takeCard() that stops the machine from taking an unlimited number of cards.

--- js/machine.js
+++ js/machine.js
@@ -1,9 +1,12 @@
 class Machine {
 	constructor() {
-		this.actions = [ 'sell', 'takeOne', 'exchange' ];
+		this.actions = [ 'sell', 'takeCard', 'cardsExchange' ];
+
 		this.sellingGoods;
 		this.cards;
 		// this.randomGood;
+
+		this.activePlayer = false;
 		this.score;
 	}
 
@@ -53,6 +56,7 @@ class Machine {
 	}
 
 	sell() {
+		this.checkCards();
 		let discardPile = document.getElementById('discard-pile');
 		// console.log(this.cards);
 
@@ -88,23 +92,27 @@ class Machine {
 				card.getAttribute('data-card') === 'silver'
 			);
 		});
-		if (bestGoods.length !== 0) {
-			// console.log('taking one of the best');
-			randomGood = bestGoods[Math.floor(Math.random() * bestGoods.length)];
-			machineDisplay.appendChild(randomGood);
-		} else {
-			// console.log('choosing a random good');
-			randomGood = marketCards[Math.floor(Math.random() * bestGoods.length)];
-			// console.log(randomGood);
-			machineDisplay.appendChild(randomGood);
+		if (machineDisplay.children.length + 1 <= 7) {
+			if (bestGoods.length !== 0) {
+				// console.log('taking one of the best');
+				randomGood = bestGoods[Math.floor(Math.random() * bestGoods.length)];
+				machineDisplay.appendChild(randomGood);
+			} else {
+				// console.log('choosing a random good');
+				randomGood = marketCards[Math.floor(Math.random() * bestGoods.length)];
+				// console.log(randomGood);
+				machineDisplay.appendChild(randomGood);
+			}
+			let cardType = deckPile.lastElementChild.getAttribute('data-card');
+
+			deckPile.lastElementChild.children[0].style.backgroundImage = `url(images/goodsCards/${cardType}.png)`;
+			deckPile.lastElementChild.classList.replace('back', 'card-container');
+			deckPile.lastElementChild.firstElementChild.setAttribute('data-card', cardType);
+			marketDisplay.appendChild(deckPile.lastChild);
 		}
-
-		let cardType = deckPile.lastElementChild.getAttribute('data-card');
-
-		deckPile.lastElementChild.children[0].style.backgroundImage = `url(images/goodsCards/${cardType}.png)`;
-		deckPile.lastElementChild.classList.replace('back', 'card-container');
-		deckPile.lastElementChild.firstElementChild.setAttribute('data-card', cardType);
-		marketDisplay.appendChild(deckPile.lastChild);
+		// else {
+		// 	console.log('too many cards');
+		// }
 	}
 
 	cardsExchange() {
@@ -120,6 +128,7 @@ class Machine {
 				card.getAttribute('data-card') === 'silver'
 			);
 		});
+
 		let cardsToSell = machineCards.filter(card => {
 			return (
 				card.getAttribute('data-card') === 'spice' ||
@@ -127,24 +136,23 @@ class Machine {
 				card.getAttribute('data-card') === 'cloth'
 			);
 		});
-		console.log(cardsToSell, cardsToTake);
+		// console.log({ cardsToSell, cardsToTake });
 		if (cardsToSell.length >= 2) {
 			if (cardsToSell.length > cardsToTake.length) {
 				let num = cardsToSell.length - cardsToTake.length;
 				cardsToSell.splice(0, num);
-				console.log(num);
+				// console.log(num);
 			} else if (cardsToTake.length > cardsToSell.length) {
 				let num = cardsToTake.length - cardsToSell.length;
 				cardsToTake.splice(0, num);
-				console.log(num);
-			} else {
-				console.log('same number of cards');
+				// console.log(num);
 			}
-		} else {
-			console.log('not enough cards');
-		}
+		} //else {
+		// 	console.log('not enough cards');
+		// }
 		// console.log('after splicing', cardsToSell, cardsToTake);
-		if (cardsToSell.length === cardsToTake.length) {
+
+		if (cardsToSell.length === cardsToTake.length && cardsToSell.length >= 2) {
 			let tempArr = cardsToSell;
 			cardsToSell = cardsToTake;
 			cardsToTake = tempArr;
@@ -152,14 +160,30 @@ class Machine {
 			cardsToSell.forEach(card => {
 				machineDisplay.appendChild(card);
 			});
+
 			cardsToTake.forEach(card => {
 				marketDisplay.appendChild(card);
 			});
+
+			// console.log('exchanging cards');
 		}
+		// else {
+		// 	console.log('change not possible');
+		// }
+	}
+
+	chooseAction() {
+		let randomAction = this.actions[Math.floor(Math.random() * this.actions.length)];
+		console.log(randomAction);
+
+		randomAction === 'sell'
+			? this.sell()
+			: randomAction === 'cardsExchange' ? this.cardsExchange() : this.takeCard();
 	}
 
 	calculateScore() {
 		let tokens = [ ...document.getElementById('machine-tokens').children ];
+
 		this.score = tokens.map(token => Number(token.getAttribute('data-value'))).reduce((acc, cur) => acc + cur, 0);
 		// console.log(tokens);
 	}
